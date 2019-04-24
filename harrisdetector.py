c20d744771021b519a1d0c2f834338b9a70cb9d2
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2019-04-24 14:24:21 +0200

    Improved speed of harrisdetector.py and added visualization function to utilities.py.

--- harrisdetector.py
+++ harrisdetector.py
@@ -1,96 +1,76 @@
 import cv2
 import numpy as np
+from scipy import signal, ndimage
+
 from operator import itemgetter
 from typing import Tuple, List
-from scipy import signal, ndimage
-from utilities import show_images
+import time
 
-def harris_corners(img: np.ndarray, threshold=1.0, blur_sigma=2.0, k=0.04, nms_n_corners=5, nms_n_bins=10, print_result=True) -> List[Tuple[float, np.ndarray]]:
+from utilities import show_images, show_image, visualize_corners
+
+def harris_corners(img: np.ndarray, threshold=1.0, blur_sigma=2.0, k=0.04, nms_bin_size=41, print_result=False) -> List[Tuple[float, np.ndarray]]:
     """
     Return the harris corners detected in the image.
     :param img: The grayscale image.
     :param threshold: The harris respnse function threshold.
     :param blur_sigma: Sigma value for the image bluring.
     :param k: Harris response trace scale factor.
-    :param nms_n_corners: Maximum number of corners in each nms bin.
-    :param nms_n_bins: Number of nms bins along each axis of the image.
+    :param nms_bin_size: Size of the bins used to perform non-maximum suppression.
     :return: A sorted list of tuples containing response value and image position.
     The list is sorted from largest to smallest response value.
     """
     # img[v,u], y-axis parallel to v-avis, x-axis parallel to u-axis
-    img_height, img_width = img.shape
+    # img_height, img_width = img.shape
 
-    # Calculate x- and y-derivative of the image using Sobel operators
-    sobel_x = np.array([[-1.0, 0.0, 1.0],
-                        [-2.0, 0.0, 2.0],
-                        [-1.0, 0.0, 1.0]])
-    sobel_y = np.array([[-1.0, -2.0, -1.0],
-                        [0.0, 0.0, 0.0],
-                        [1.0, 2.0, 1.0]])
-    img_x = signal.convolve2d(img, sobel_x, mode='same', boundary='symm')
-    img_y = signal.convolve2d(img, sobel_y, mode='same', boundary='symm')
+    time_start = time.time()
 
-    # Compute products of derivatives
-    img_xx = np.multiply(img_x, img_x)
-    img_yy = np.multiply(img_y, img_y)
-    img_xy = np.multiply(img_x, img_y)
-    
+    if nms_bin_size % 2 == 0:
+        nms_bin_size += 1
+
+    if nms_bin_size < 3:
+        nms_bin_size = 3
+
+    nms_bin_size = (nms_bin_size, nms_bin_size)
+
+    # Calculate x- and y-derivative of the image using Scharr
+    img_x = cv2.Scharr(src=img, ddepth=-1, dx=1, dy=0)
+    img_y = cv2.Scharr(src=img, ddepth=-1, dx=0, dy=1)
+
     # Blur products of derivatives
-    blur_img_xx = ndimage.filters.gaussian_filter(img_xx, blur_sigma)
-    blur_img_yy = ndimage.filters.gaussian_filter(img_yy, blur_sigma)
-    blur_img_xy = ndimage.filters.gaussian_filter(img_xy, blur_sigma)
-
-    # Set up the structure tensor
-    first_row = np.stack([blur_img_xx, blur_img_xy], axis=2)
-    second_row = np.stack([blur_img_xy, blur_img_yy], axis=2)
-    structure_tensor = np.stack([first_row, second_row], axis=2)
-
-    # Compute structure matrices determinants and traces
-    dets = np.linalg.det(structure_tensor)
-    traces = np.trace(structure_tensor, axis1=2, axis2=3)
+    img_xx = ndimage.filters.gaussian_filter(img_x * img_x, blur_sigma)
+    img_yy = ndimage.filters.gaussian_filter(img_y * img_y, blur_sigma)
+    img_xy = ndimage.filters.gaussian_filter(img_x * img_y, blur_sigma)
 
     # Compute Harris response
-    responses = dets - k*np.square(traces)
+    dets = img_xx * img_yy - img_xy * img_xy
+    traces = img_xx + img_yy
+    responses = dets - k * traces * traces
+
+    # Perform non-maximum suppression
+    nms_kernel = np.ones(nms_bin_size)
+    is_not_maxima = np.logical_not(responses == cv2.dilate(responses, nms_kernel, iterations=1))
+    responses[is_not_maxima] = threshold - 1
 
     # Threshold based on response value
-    img_indices = np.argwhere(responses > threshold)
-    positions = np.flip(img_indices, axis=1)
-    responses = responses[np.where(responses > threshold)]
-    
-    # Sort responses and corners based on response
-    filter_indices = np.flip(np.argsort(responses))
-    positions = np.take(positions, filter_indices, axis=0)
-    responses = np.take(responses, filter_indices)
-    
-    # Perform non-maximum suppression
-    bin_height = int(np.ceil(img_height / nms_n_bins))
-    bin_width = int(np.ceil(img_width / nms_n_bins))
-    bin_size = np.array([bin_height, bin_width])
-    bin_positions = (positions / bin_size).astype(int)
+    mask = responses > threshold
+    filtered_positions = np.flip(np.argwhere(mask), axis=1)
+    filtered_responses = responses[mask]
 
-    nms_indices = []
-    nonempty_bins = np.unique(bin_positions, axis=0)
-    bins = np.full((nonempty_bins.shape[0], nms_n_corners), np.nan)
+    # Sort responses and corners based on response value
+    sorting_indices = np.flip(np.argsort(filtered_responses))
+    sorted_positions = np.take(filtered_positions, sorting_indices, axis=0)
+    sorted_responses = np.take(filtered_responses, sorting_indices)
 
-    for index, bin_position in enumerate(bin_positions):
-        bin_index = np.argwhere(np.all(nonempty_bins==bin_position, axis=1))[0][0]
-        bin_values = bins[bin_index]
-        is_not_nan = np.logical_not(np.isnan(bin_values))
+    corners = [(sorted_responses[i], sorted_positions[i]) for i in range(len(sorted_positions))]
 
-        n_neighbours = np.sum(is_not_nan, dtype=int)
-
-        if n_neighbours < nms_n_corners:
-            valid_index = np.argwhere(np.isnan(bin_values))[0][0]
-            bins[bin_index][valid_index] = index
-            nms_indices.append(index)
-
-    nms_indices = np.array(nms_indices)
-    nms_positions = np.take(positions, nms_indices, axis=0)
-    nms_responses = np.take(responses, nms_indices)
-
-    corners = [(nms_responses[i], nms_positions[i]) for i in range(len(nms_positions))]
+    time_stop = time.time()
 
     if print_result:
+        time_total = time_stop - time_start
         print("Harris corner detector detected {} corners...".format(len(corners)))
+        print("Total time: {:>8}".format(time_total))
+        print("FPS: {}".format(1/time_total))
+        visualize_corners(img, sorted_positions.T)
+        input("Press a key to continue...")
 
     return corners
--- utilities.py
+++ utilities.py
@@ -1,5 +1,8 @@
 import numpy as np
+import cv2
 import matplotlib.pyplot as plt
+
+from typing import Tuple, List
 
 def show_images(imgs, titles=[], n_cols=3):
     n_imgs = len(imgs)
@@ -20,3 +23,17 @@ def show_images(imgs, titles=[], n_cols=3):
     mng.resize(*mng.window.maxsize())
     plt.show()
 
+def show_image(img):
+    cv2.imshow("Frame", img)
+
+def visualize_corners(img: np.ndarray, points: np.ndarray, radius=5, color=(255,0,0), thickness=2):
+    """
+    Visualizes the rgb image and the Harris corners that are detected in it.
+    """
+    img_copy = img.copy()
+    
+    for point in points.T:
+        indices = point.astype(int)
+        cv2.circle(img_copy, tuple(indices), radius, color, thickness)
+    
+    cv2.imshow("Frame", img_copy)
